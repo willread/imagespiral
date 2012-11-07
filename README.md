a74dd8fec89085735e93c762353f3203c68bc2e1
William Blanchette <wb@collectivecognition.com>
2012-11-07 15:24:04 -0700

    Work in progress commit.

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-pornempire
-==========
+Nothing to see here. Move along.
--- src/app.js
+++ src/app.js
@@ -1,41 +1,71 @@
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path')
-  , fs = require("fs"),
-	im = require("imagemagick");
-	
-// TODO: Stats? Probably just use google analytics or something, no need for a custom solution right now
+var ads = require("./config/ads");
 
+var express = require("express");
+var routes = require("./routes");
+var user = require("./routes/user");
+var http = require("http");
+var path = require("path");
+var fs = require("fs");
+var im = require("imagemagick");
+	
 var app = express();
 
+/*
+ *
+ * App configurations
+ *
+ */
+
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser('your secret here'));
-  app.use(express.session());
-  app.use(app.router);
-  app.use(require('less-middleware')({ src: __dirname + '/public' }));
-  app.use(express.static(path.join(__dirname, 'public')));
+	app.set('port', process.env.PORT || 3000);
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	
+	app.use(express.cookieParser("I aM A ReaLLy DiffiCulT TO GueSS SecReT"));
+	app.use(express.session());
+	app.use(express.static(path.join(__dirname, 'public')));
+	
+	app.use(function(req, res, next){
+		
+		console.log("i got called!");
+
+		
+		res.locals.ad = "There is no ad";
+		next();
+
+	});
+	
+	app.use(express.favicon());
+	app.use(express.logger('dev'));
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(require('less-middleware')({ src: __dirname + '/public' }));
 });
 
 app.configure('development', function(){
 	app.use(express.errorHandler());
 });
 
+/*
+ *
+ * Routes
+ *
+ */
+
 app.get('/', routes.index);
 app.get('/users', user.list);
+
+/*
+ *
+ * Single image view
+ *
+ */
 
 app.get("/:token", function(req, res, next){
 	fs.exists("./public/images/" + req.params.token + ".jpg", function(exists){
 		if(!exists)	
-			res.send("not found: " + req.params.token); // TODO: Proper 404
+			res.send("not found"); // TODO: Proper 404
 		else
 			res.render("single", {
 				token: req.params.token,
@@ -45,8 +75,13 @@ app.get("/:token", function(req, res, next){
 	});
 });
 
-app.post("/", function(req, res){
+/*
+ *
+ * Handle image uploads
+ *
+ */
 
+app.post("/", function(req, res){
 	var token = Math.floor(Math.random()*16777215).toString(16); // TODO: Generate better token
 
 	var temp = req.files.file.path;
@@ -90,8 +125,13 @@ app.post("/", function(req, res){
 	}catch(err){
 		console.log("err: " + err);
 	}
-	
 });
+
+/*
+ *
+ * Start the server!
+ *
+ */
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
