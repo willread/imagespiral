1bf1ae12fb8f83db91e2f9ed5da9c317e23e63e4
William Blanchette <wb@collectivecognition.com>
2012-11-19 09:18:29 -0700

    Removed last vestiges of imagemagick wrapper code.

--- src/app.js
+++ src/app.js
@@ -130,72 +130,23 @@ app.post("/", function(req, res){
 	if(!temp){
 		res.send("error");
 	}else{
-	
-		// TODO: Convert all to JPEGs
-		// TODO: Strip metadata
-		// TODO: Validate
-		
-		try {
-			
-			im.identify(temp, function(err, features){
-				
-				if(features && features.format == "JPEG"){
-					
-					/*
-					im.crop({
-						srcPath: temp,
-						dstPath: "./public/thumbs/" + token + ".jpg",
-						width: 150,
-						height: 150,
-						quality: 1,
-						gravity: "Center"
-					}, function(err, stdout, stderr){
-						if(err) throw err;
-						console.log(arguments);
-						fs.rename(temp, path + ".jpg", function(err){
-							if(err) throw err;
-							
+
+		var child = exec("convert " + temp + " -strip -thumbnail 250x250 " + "./public/small/" + token + ".jpg", function (error, stdout, stderr){
+			// console.log(error);
+			// console.log(stdout);
+			// console.log(stderr);
+			fs.exists("./public/small/" + token + ".jpg", function(exists){
+				if(exists){
+					exec("convert " + temp + " -strip -thumbnail 600x600 " + "./public/thumbs/" + token + ".jpg", function (error, stdout, stderr){
+						exec("convert " + temp + " -strip " + "./public/images/" + token + ".jpg", function (error, stdout, stderr){
 							res.redirect("/" + token);
-						})	
-					});
-					*/
-					/*im.resize({
-						srcPath: temp,
-						dstPath: "./public/thumbs/" + token + ".jpg",
-						width: 600,
-						quality: 1,
-						gravity: "Center"
-					}, function(err, stdout, stderr){
-						if(err) throw err;
-						console.log(arguments);
-						fsextra.copy(temp, path + ".jpg", function(err){
-							if(err) throw err;
-							
-							res.redirect("/" + token);
-						})	
-					});*/
-					// convert wallpaper019-1920x1200.jpg -strip -thumbnail 250x250 thumb.jpg
-					var child = exec("convert " + temp + " -strip -thumbnail 250x260 " + "./public/small/" + token + ".jpg", function (error, stdout, stderr){
-						// console.log(error);
-						// console.log(stdout);
-						// console.log(stderr);
-						exec("convert " + temp + " -strip -thumbnail 600x600 " + "./public/thumbs/" + token + ".jpg", function (error, stdout, stderr){
-							exec("convert " + temp + " -strip " + "./public/images/" + token + ".jpg", function (error, stdout, stderr){
-								res.redirect("/" + token);
-							});
 						});
 					});
 				}else{
-				
-					res.send("error"); // FIXME
-				
+					res.send("error");
 				}
-				
 			});
-		
-		}catch(err){
-			console.log("err: " + err);
-		}
+		});
 	
 	}
 
